9e895251cb8c7826a6aea890a8e63cfaa24e6df9
Andrii <farikpite@gmail.com>
2019-10-20 14:52:52 +0300

    Solution

---
+++ script.js
@@ -0,0 +1,17 @@
+function daysInYear(year) {
+    try{
+        if (year !== Number || !Number.isInteger(year)) {
+            throw new Error();
+        } 
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? console.log(366) : console.log(365);
+    } catch (e) {
+        return 'exception, it is not a year!'
+    }
+}
+
+daysInYear(2019); // 365
+// daysInYear('2019'); // exception
+// daysInYear(2020); // 366
+// daysInYear(2020.2); // exception
+// daysInYear(2100); // 365
+// daysInYear({year: 2100}); // exception
